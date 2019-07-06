da6cfca7047f33fb897e8f46546faaa4ba27ce48
smithros <kovalr2000@gmail.com>
2019-07-07 00:14:09 +0300

    added prime factors searching algotithm

---
+++ prime_factors.py
@@ -0,0 +1,17 @@
+def prime_factors(number):
+    var = 2
+    factors = []
+    while var * var <= number:
+        if number % var:
+            var += 1
+        else:
+            number //= var
+            factors.append(var)
+    if number > 1:
+        factors.append(number)
+    return factors
+
+
+if __name__ == "__main__":
+    n = 22
+    print(prime_factors(n))
